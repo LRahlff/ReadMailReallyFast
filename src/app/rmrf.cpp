d1cfddb1714bbfe5a93a8a9f19e5236ffcc9b088
Benny Baumann <BenBE@geshi.org>
2019-09-29 02:46:55 +0200

    add: Actually use gettext

--- src/app/rmrf.cpp
+++ src/app/rmrf.cpp
@@ -1,14 +1,23 @@
+#include <libintl.h>
+#include <locale.h>
+
 #include <iostream>
 
 #include "ui/view.hpp"
 
+#define _(str) gettext(str)
+
 int main() {
     using rmrf::ui::display;
+
+    setlocale(LC_ALL, NULL);
+    bindtextdomain("rmrf", "/usr/share/locale/");
+    textdomain("rmrf");
 
     auto h_nc = std::make_shared<display>();
 
     h_nc->sync([](const display::ptr_type &) {
-        std::cout << "Hello World!\n" << std::flush;
+        std::cout << _("Hello World!\n") << std::flush;
     });
 
     return 0;
